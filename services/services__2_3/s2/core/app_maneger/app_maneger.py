4d82a28615fdf8d7e6df7320599273ae94c02c4c
solhope23 <s526413103@gmail.com>
2025-08-27 21:48:01 +0300

    cange_logic_topic_name

--- services/services__2_3/s2/core/app_maneger/app_maneger.py
+++ services/services__2_3/s2/core/app_maneger/app_maneger.py
@@ -16,7 +16,7 @@ class AppManager:
             self.client.open()
             for msg in self.dal.consume():
                 print("The pull from Kafka was successful.")
-                data = DataBuilder.build(msg)
+                data = DataBuilder.build(msg, self.topic_client_names)
                 self.client.send_to_kapka(data['topic_name'], data['message'])
                 print("'The sending was successful.")
         except Exception as e:
